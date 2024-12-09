795d56aeaea775739a72c4b31588da0355519092
Thea Calaquian <tc.11927@Tristans-MacBook-Pro.local>
2024-12-09 15:37:42 -0800

    q3

---
+++ q3.js
@@ -0,0 +1,86 @@
+const { userInfo } = require("os");
+
+const readline = require("readline").createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+/*
+This will be a simple application, but potentially complicated to implement. There's a set of colors in the theme object. "red", "green", "blue", "yellow", and "orange". By default they are all true. The application allow users to add a color to the system as long as it's part of the 5 colors. You can toggle the colors from true to false with the command "toggle" and then a second readline for the color itself. Always DisplayUserColors after AddUserColor or ToggleThemeColor completes.
+
+Here are some logistics that this application must follow
+Only add a color when the theme color is true otherwise console log that it's not allowed
+When a color is toggled from true to false, also remove the color from userColors. You can do this by making a new array, then looping through userColors and only pushing the colors that are true into the new array. Then reassign the new array to userColors.
+*/
+
+let userColors = [];
+let theme = {
+  //the 5 color and their boolean value goes here
+  red:true,
+  green:true,
+  blue:true,
+  yellow:true,
+  orange:true,
+};
+
+//rename this to AddUserColor
+function AddUserColor(){
+  readline.question("What color would you like to add?", _color=>{
+    let checkColorAllowed = false;
+    for(let key in theme){
+      if(_color === key && theme[key] === true){
+        checkColorAllowed = true;
+        userColors.push(_color)
+      }
+    } console.log ("your colors:")
+    console.log(theme);
+    StartApp();
+  })
+  //add a color to userColors
+}
+
+//rename this to DisplayUserColors
+function DisplayUserColors(){ 
+  console.log ("your colors:")
+    console.log(userColors);
+    StartApp();
+}
+
+
+//rename this to ToggleThemeColor
+function ToggleThemeColor(){
+  readline.question("Which color do you want to toggle?", _command =>{
+  if(_command === "toggle red"){
+    theme.red = !theme.red;
+  } else if(_command === "toggle green"){
+    theme.green = !theme.green;
+  } else if(_command === "toggle blue"){
+    theme.blue = !theme.blue;
+  } else if(_command === "toggle yellow"){
+    theme.yellow = !theme.yellow;
+  } else if(_command === "toggle orange"){
+    theme.orange = !theme.orange;
+  }
+  console.log ("your colors:")
+    console.log(theme);
+    StartApp();
+
+  })
+  //ask for a color to toggle
+} 
+
+
+function StartApp(){
+  readline.question("What is your command? ", _command=>{
+    if(_command === "quit"){
+      readline.close();
+    } else if (_command === "add"){
+      AddUserColor();
+    } else if (_command === "show"){
+      DisplayUserColors();
+    } else if(_command === "toggle"){
+      ToggleThemeColor();
+    }
+  })
+}
+
+StartApp();
